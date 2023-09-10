64d0ead731cf95aa3f87ea8ac0d33251fabfbc4d
SomAndr92 <nikmos92@mail.ru>
2023-09-10 10:18:40 +0300

    v2

--- script.js
+++ script.js
@@ -23,18 +23,14 @@ class pokeapi {
 
     getPoke(name) {
 
-
         if (name != "") {
-
 
             fetch('https://pokeapi.co/api/v2/pokemon/' + name)
                 .then((response) => response.json())
                 .then((data) => {
 
-
                     // let name = document.getElementById("card");
                     // name.innerHTML += "<h5 class="+'card-title'+">"+data["species"]["name"]+"</h5>" //"Имя: " + data["species"]["name"];
-
 
                     // let sprite = document.getElementById("carddb");
                     /*if (data["sprites"]["front_default"] == null) {
@@ -44,7 +40,7 @@ class pokeapi {
 
 
                     let block = '<div class="card bg-secondary" style="width: 18rem;" id="card">' +
-                        "<h5 class=" + 'card-title' + ">" + data["species"]["name"] + "</h5>" +
+                        "<h5 class=" + 'card-title ' + ">" + data["species"]["name"] + "</h5>" +
                         '<div class="card-body" id="carddb">' +
                         '<img class="card-img-top" src="' + data["sprites"]["other"]["dream_world"]["front_default"] + '"></img>' +
                         '</div>' +
